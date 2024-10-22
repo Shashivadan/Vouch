f16e866e32c5deadbb5f5fe33bd73d69e247915c
Shashivadan <shashivadan99@gmail.com>
2024-10-22 23:07:49 +0530

    feat: added opgrah and some other stuff

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -2,34 +2,47 @@ import type { Metadata, Viewport } from "next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
+
+
 import { cn } from "@acme/ui";
 import { ThemeProvider, ThemeToggle } from "@acme/ui/theme";
 import { Toaster } from "@acme/ui/toast";
 
+
+
 import { TRPCReactProvider } from "~/trpc/react";
 
-import "~/app/globals.css";
+
+
+import "~/styles/globals.css";
+
+
 
 import { env } from "~/env";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL(
     env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
+      ? "https://vouch.lab-x.xyz"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  title: "Vouch",
+  icons: "/icons/icon.png",
+  description:
+    "Vouch is a zero-code platform that transforms the way businesses collect and showcase customer testimonials.",
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    title: "Vouch",
+    description:
+      "Vouch is a zero-code platform that transforms the way businesses collect and showcase customer testimonials.",
+    url: "https://vouch.lab-x.xyz",
+    type: "website",
+    siteName: "Vouch",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+    site: "@shashivadan99",
+    creator: "@shashivadan99",
   },
 };
 
